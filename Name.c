846d13980158e6fec5899cac5bb37626ff7fdbc8
bimalenduz1000 <bimalenduz1000@gmail.com>
2025-02-15 04:53:33 +0530

    Initial Update

---
+++ Name.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void main(){
+  char name[100];
+  printf("Enter your name:");
+  scanf("%s",name);
+  printf("Your name is:%s",name);
+}
